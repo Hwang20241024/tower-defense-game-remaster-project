824edeaa27f21420c61b1ebbd44dcf9cfd81b4df
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2025-01-21 21:45:14 +0900

    [feat] ondata 패킷 헤더 파싱
    패킷 헤더 파싱
    페이로드 파싱은 테스트중

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,4 +1,52 @@
-export const onData = (socket) => async (data) => {  
+import { getProtoMessages } from "../init/loadProtos.js";
+import { getProtoTypeNameByHandlerId } from '../handlers/index.js';
+
+export const onData = (socket) => async (data) => {
   console.log('클라이언트가 데이터를 보냈습니다.');
   // 클라이언트 데이터 처리 로직 추가.
+  console.log(data);
+
+  socket.buffer = Buffer.concat([socket.buffer, data]);
+
+  let offset = 0;
+  const packetType = socket.buffer.readInt16BE(offset);
+  offset += 2;
+  console.log(`packetType : ${packetType}`);
+
+  const versionLength = data.readInt8(offset);
+  offset += 1;
+  console.log(`versionLength : ${versionLength}`);
+
+  const version = data.toString('utf-8', offset, offset + versionLength);
+  offset += versionLength;
+  console.log(`version : ${version}`);
+
+  const sequence = data.readInt32BE(offset);
+  offset += 4;
+  console.log(`sequence : ${sequence}`);
+
+  const payloadLength = data.readInt32BE(offset);
+  offset += 4;
+  console.log(`payloadLength : ${payloadLength}`);
+
+  const payload = data.slice(offset, offset + payloadLength);
+  console.log(`payload : ${payload}`);
+
+  const packet = { packetType, versionLength, version, sequence, payloadLength, payload };
+  console.log(packet);
+
+  /*
+    const protoMessages = getProtoMessages();
+    const protoTypeName = getProtoTypeNameByHandlerId(packetType);
+    const [namespace, typeName] = protoTypeName.split('.');
+  
+    const PayloadType = protoMessages[namespace][typeName];
+  
+    let pl;
+    pl = PayloadType.decode(packet.payload);
+  
+    const id = pl.id;
+    const password = pl.password;
+    console.log(id, password);
+    */
 };
