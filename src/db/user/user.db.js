eacab0af07fed0f2569c6e115ac8468432e32cfc
EuiJoong <euijki87@gmail.com>
2025-01-22 15:58:43 +0900

    fix : 유저 조회 수정

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -1,10 +1,11 @@
 import pools from '../database.js';
 import { SQL_USER_QUERIES } from './user.queries.js';
 
-const createUser = async (id, password, email) => {
+export const createUser = async (id, password, email) => {
   await pools.CH5_TEAM.query(SQL_USER_QUERIES.CREATE_USER, [id, password, email]);
 };
 
-const findUserById = async (id) => {
-  await pools.CH5_TEAM.query(SQL_USER_QUERIES.FIND_USER_BY_ID, [id]);
+export const findUserById = async (id) => {
+  const [rows] = await pools.CH5_TEAM.query(SQL_USER_QUERIES.FIND_USER_BY_ID, [id]);
+  return rows[0];
 };
