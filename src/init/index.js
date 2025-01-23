08ea7c26a7558e28caeda64ed782f194d54f87df
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2025-01-23 15:54:48 +0900

    [feat] DB test connection
    -서버 시작 시 db 연결 확인

--- src/init/index.js
+++ src/init/index.js
@@ -1,12 +1,14 @@
 // 서버 초기화 작업
 import { loadProtos } from "./loadProtos.js";
 import MonsterManager from "../classes/managers/monster.manager.js";
+import { testAllConnections } from "../utils/db/testConnection.js";
+import pools from "../db/database.js";
 
 const initServer = async () => {
   try {
     // 초기화
     await loadProtos();     // 프로토파일 읽기.
-
+    await testAllConnections(pools); // DB 연결 테스트
     // 초기화 작업 추가.
 
   } catch (e) {
