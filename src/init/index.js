21fd8067c2d092b4bcb3eb36ec2ae09afaf84f59
EuiJoong <euijki87@gmail.com>
2025-01-23 17:53:51 +0900

    feat : 서버 실행 시 전체 유저의 로그인 상태 초기화 추가

--- src/init/index.js
+++ src/init/index.js
@@ -1,14 +1,17 @@
 // 서버 초기화 작업
-import { loadProtos } from "./loadProtos.js";
-import MonsterManager from "../classes/managers/monster.manager.js";
+import { loadProtos } from './loadProtos.js';
+import MonsterManager from '../classes/managers/monster.manager.js';
+import { updateAllUserLoginState } from '../db/user/user.db.js';
 
 const initServer = async () => {
   try {
     // 초기화
-    await loadProtos();     // 프로토파일 읽기.
+    await loadProtos(); // 프로토파일 읽기.
 
     // 초기화 작업 추가.
 
+    // 유저 로그인 상태 초기화
+    await updateAllUserLoginState();
   } catch (e) {
     console.error(e);
     process.exit(1); // 오류 발생 시 프로세스 종료
