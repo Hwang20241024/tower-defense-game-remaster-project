6c97d5e2114502766c075ad6e454429fdf96c4f6
EuiJoong <euijki87@gmail.com>
2025-01-23 16:16:55 +0900

    feat : 로그인/회원가입에 대한 분기처리 추가

--- src/utils/error/errorHandler.js
+++ src/utils/error/errorHandler.js
@@ -1,19 +1,47 @@
 import { createResponse } from '../response/createResponse.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+import { PACKET_TYPE } from '../../constants/header.js';
 
-export const handleError = (socket, error) => {
-  let responseCode;
-  let message;
+export const handleError = (socket, error, packetType = null) => {
+  let failCode;
 
   if (error.code) {
-    responseCode = error.code;
-    message = error.message;
     console.error(`에러 코드: ${error.code}, 메시지: ${error.message}`);
+    failCode = 2;
   } else {
-    responseCode = 10000; // 일반 에러 코드
-    message = error.message;
+    failCode = 1;
     console.error(`일반 에러: ${error.message}`);
   }
-  
-  const errorResponse = createResponse(-1, responseCode, { message }, null);
-  socket.write(errorResponse);
+
+  if (packetType !== null) {
+    const protoMessages = getProtoMessages();
+    const response = protoMessages.towerDefense.GamePacket;
+    let gamePacket;
+    let responsePacketType;
+    if (PACKET_TYPE.REGISTER_REQUEST === packetType) {
+      //회원 가입
+      gamePacket = response.create({
+        registerResponse: {
+          success: false,
+          message: error.message,
+          failCode: failCode,
+        },
+      });
+      responsePacketType = PACKET_TYPE.REGISTER_RESPONSE;
+    } else if (PACKET_TYPE.LOGIN_REQUEST === packetType) {
+      //로그인
+      gamePacket = response.create({
+        loginResponse: {
+          success: false,
+          message: error.message,
+          token: null,
+          failCode: failCode,
+        },
+      });
+      responsePacketType = PACKET_TYPE.LOGIN_RESPONSE;
+    }
+    const responsePayload = response.encode(gamePacket).finish();
+    const responsePacket = createResponse(responsePacketType, 0, responsePayload);
+    socket.write(responsePacket);
+  }
 };
