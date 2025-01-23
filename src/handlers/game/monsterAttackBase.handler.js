7d14fef164461b4c4fea5908bd269ba9ef8e6c34
ppiok-OwO <solcorin@naver.com>
2025-01-23 13:00:31 +0900

    🩹 fix 아직 구현되지 않은 코드 주석 처리

--- src/handlers/game/monsterAttackBase.handler.js
+++ src/handlers/game/monsterAttackBase.handler.js
@@ -25,21 +25,21 @@ export const monsterAttackBaseHandler = ({ socket, payload }) => {
   // 클라이언트에서 에셋파일로 몬스터의 데미지를 정의하고 있기 때문에 config에 하드코딩해둔 값으로 검증하거나 json 파일을 만들어야 함
 
   // 2. 몬스터의 위치가 정말로 endPoint에 다다랐는가?(최소한의 검증)
-  let monsterPos; // 이걸 서버에서 상태 동기화 중인 몬스터 좌표라고 가정
-  // 근데 payload에 몬스터 ID가 없어서...가장 오른쪽에 도달한 몬스터의 좌표를 가져와야할 것 같음
-  let endPoint; // 서버에서 생성한 monsterPath의 마지막 좌표라고 가정
+  // let monsterPos; // 이걸 서버에서 상태 동기화 중인 몬스터 좌표라고 가정
+  // // 근데 payload에 몬스터 ID가 없어서...가장 오른쪽에 도달한 몬스터의 좌표를 가져와야할 것 같음
+  // let endPoint; // 서버에서 생성한 monsterPath의 마지막 좌표라고 가정
 
-  const distance = Math.sqrt(
-    Math.pow(monsterPos.x - endPoint.x, 2) + Math.pow(gate.position.y - user.y, 2),
-  ); // 서버에서 업데이트 중인 몬스터의 좌표와 실제 엔드 포인트의 좌표 사이의 거리
+  // const distance = Math.sqrt(
+  //   Math.pow(monsterPos.x - endPoint.x, 2) + Math.pow(gate.position.y - user.y, 2),
+  // ); // 서버에서 업데이트 중인 몬스터의 좌표와 실제 엔드 포인트의 좌표 사이의 거리
 
-  // 거리 distance가 오차범위를 넘어서면 에러라고 판단
-  if (distance > config.ingame.offset) {
-    // throw new CustomError(
-    //   ErrorCodes.INVALID_POSITON,
-    //   '몬스터가 아직 베이스에 도착하지 않았습니다.',
-    // );
-  }
+  // // 거리 distance가 오차범위를 넘어서면 에러라고 판단
+  // if (distance > config.ingame.offset) {
+  //   // throw new CustomError(
+  //   //   ErrorCodes.INVALID_POSITON,
+  //   //   '몬스터가 아직 베이스에 도착하지 않았습니다.',
+  //   // );
+  // }
 
   user.baseHp -= damage;
   if (user.baseHp <= 0) {
@@ -65,5 +65,6 @@ export const monsterAttackBaseHandler = ({ socket, payload }) => {
 
     // 게임 승패가 결정되는 동시에 게임 종료 작업
     removeGameSession(gameId);
+    user.setGameId(null);
   }
 };
