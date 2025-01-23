d5cab4c8de62cddba57c031edf60d0439721f5e7
yoon-H <ucando0320@gmail.com>
2025-01-23 12:18:15 +0900

    feat : add syncState function in user class

--- src/classes/models/user.class.js
+++ src/classes/models/user.class.js
@@ -1,5 +1,7 @@
 import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import CustomError from '../../utils/error/customError.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+import { PACKET_TYPE } from '../../constants/header.js';
 
 class User {
   constructor(socket, sequence) {
@@ -7,6 +9,8 @@ class User {
     this.sequence = sequence;
     this.baseHp = 100;
     this.score = 0;
+    this.userGold = 10;
+    this.monsterLevel = 1;
   }
 
   setGameId(gameId) {
@@ -27,6 +31,30 @@ class User {
 
   getNextSequence() {
     return ++this.sequence;
+  }
+
+  syncStateNotification() {
+    const protoMessages = getProtoMessages();
+    const notification = protoMessages.towerDefense.GamePacket;
+    const notificationGamePacket = notification.create({
+      stateSyncNotification: {
+        userGold: this.userGold,
+        baseHp: this.baseHp,
+        monsterLevel: this.monsterLevel,
+        score: this.score,
+        message: '타워가 생성되었습니다.',
+      },
+    });
+
+    const notificationPayload = notification.encode(notificationGamePacket).finish();
+
+    const towerPurchaseResponse = createResponse(
+      PACKET_TYPE.STATE_SYNC_NOTIFICATION,
+      this.sequence,
+      notificationPayload,
+    );
+
+    this.socket.write(towerPurchaseResponse);
   }
 }
 
