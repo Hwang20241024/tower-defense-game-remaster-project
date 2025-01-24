1fbd94b7035119fc81a3811625f1bb4a63042510
Hwang20241024 <ghkdaksdbs@naver.com>
2025-01-24 10:36:59 +0900

    fix: 몬스터 클래스 검증용 체력 추가/ 몬스터 사망시 점수획득 변경

--- src/handlers/monsterDeath.handler.js
+++ src/handlers/monsterDeath.handler.js
@@ -3,38 +3,41 @@ import { getGameSession } from '../session/game.session.js';
 import { getUserBySocket } from '../session/user.session.js';
 import { PACKET_TYPE } from '../constants/header.js';
 import { createResponse } from '../utils/response/createResponse.js';
+import { config } from '../config/config.js';
 
 const monsterDeathHandler = async (socket, payload) => {
   // 저장되어 있는 몬스터 삭제.
   const gameId = getUserBySocket(socket);
   const gameSession = getGameSession(gameId.getGameId());
-  gameSession.removeMonster(payload.monsterId);
-  gameId.addScore(100);
 
-  // 페이로드 직렬화.
-  const protoMessages = getProtoMessages();
+  // 몬스터 삭제하고 점수 갱신.
+    gameSession.removeMonster(payload.monsterId);
+    gameId.addScore(config.ingame.score * gameId.getMonsterLevel());
 
-  const response = protoMessages.towerDefense.GamePacket;
-  const gamePacket = response.create({
-    enemyMonsterDeathNotification: {
-      monsterId: payload.monsterId,
-    },
-  });
+    // 페이로드 직렬화.
+    const protoMessages = getProtoMessages();
 
-  const payloadData = response.encode(gamePacket).finish();
+    const response = protoMessages.towerDefense.GamePacket;
+    const gamePacket = response.create({
+      enemyMonsterDeathNotification: {
+        monsterId: payload.monsterId,
+      },
+    });
 
-  // "헤더 + 페이로드" 직렬화.
-  const initialResponse = createResponse(
-    PACKET_TYPE.ENEMY_MONSTER_DEATH_NOTIFICATION,
-    gameId.sequence,
-    payloadData,
-  );
+    const payloadData = response.encode(gamePacket).finish();
 
+    // "헤더 + 페이로드" 직렬화.
+    const initialResponse = createResponse(
+      PACKET_TYPE.ENEMY_MONSTER_DEATH_NOTIFICATION,
+      gameId.getNextSequence(),
+      payloadData,
+    );
 
-  // socket.write(initialResponse);
+    // 브로드 케스트 추가
+    await gameSession.broadcast(initialResponse, socket);
 
-  // 브로드 케스트 추가
-  await gameSession.broadcast(initialResponse, socket);
+  
+
 };
 
 export default monsterDeathHandler;
